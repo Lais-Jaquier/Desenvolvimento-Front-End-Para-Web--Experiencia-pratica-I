36fb1cfb4b2bbcb7eab8bfe2f1983178362ec3ad
Lais-Jaquier <laisjaquier@gmail.com>
2025-11-05 21:09:39 -0300

    "primeiro commit"

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("formCadastro").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let cpf = document.getElementById("cpf").value;
+    let telefone = document.getElementById("telefone").value;
+    let nascimento = document.getElementById("nascimento").value;
+    let endereco = document.getElementById("endereco").value;
+    let cep = document.getElementById("cep").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+
+    alert(
+        "Cadastro enviado com sucesso!\n\n" +
+        "Nome: " + nome + "\n" +
+        "Email: " + email + "\n" +
+        "CPF: " + cpf + "\n" +
+        "Telefone: " + telefone + "\n" +
+        "Nascimento: " + nascimento + "\n" +
+        "Endereço: " + endereco + "\n" +
+        "CEP: " + cep + "\n" +
+        "Cidade: " + cidade + "\n" +
+        "Estado: " + estado
+    );
+});
